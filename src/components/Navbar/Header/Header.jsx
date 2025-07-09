a1809beb5ee8445ffd5e56cb3c940e2d03d6913d
shivam95200 <shivam.sisodiya_cs22@gla.ac.in>
2025-07-09 23:30:29 +0530

    day3

---
+++ src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order Your favourite Food here!</h2>
+            <p>choose from diverse variety</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../components/Navbar/Header/Header'
 const Home = () => {
   return (
-    <div>Home</div>
+    <Header/>
   )
 }
 
