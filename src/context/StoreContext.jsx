f5c14c6070bb24f24eec2671abcf1fc201d5ba22
shivam95200 <shivam.sisodiya_cs22@gla.ac.in>
2025-07-12 23:35:00 +0530

    day4

---
+++ src/context/StoreContext.jsx
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+export const StoreContext=createContext(null);
+
+const StoreContextProvider=()=>{
+
+    const contextValue={
+        
+    }
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import Header from '../../components/Navbar/Header/Header'
+import ExploreMenu from '../../components/Navbar/ExploreMenu/ExploreMenu'
 const Home = () => {
+  const[category,setCategory]=useState("All");
+
   return (
+    <div>
     <Header/>
+    <ExploreMenu category={category} setCategory={setCategory}/>
+    </div>
   )
 }
 
