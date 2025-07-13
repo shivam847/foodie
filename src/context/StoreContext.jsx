5e3f0235f47567a5bb8133fb972ad1acdf168ff4
shivam847 <shivamsisodiya31072004@gmail.com>
2025-07-13 22:11:16 +0530

    Home Page

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,10 +1,38 @@
 import { createContext } from "react";
 
-export const StoreContext=createContext(null);
+export const StoreContext = createContext(null);
+import { food_list } from "../assets/assets";
 
-const StoreContextProvider=()=>{
+const StoreContextProvider = (props) => {
 
-    const contextValue={
-        
+    // const [cartItems, setCartItems] = useState({});
+
+    // const addToCart = (itemId) => {
+    //     if (!cartItems[itemId]) {
+    //         setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
+    //     }
+    //     else {
+    //         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+    //     }
+    // }
+    // const removeFromCart = (itemId) => {
+    //     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    // }
+
+    // useEffect(() => {
+    //     console.log(cartItems);
+    // }, [cartItems])
+
+    const contextValue = {
+        food_list
+        // cartItems,
+        // setCartItems,
+        // addToCart,
+        // removeFromCart
     }
+    return <StoreContext.Provider value={contextValue}>
+        {props.children}
+    </StoreContext.Provider>
 }
+
+export default StoreContextProvider
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Home.css'
 import Header from '../../components/Navbar/Header/Header'
 import ExploreMenu from '../../components/Navbar/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/Navbar/FoodDisplay/FoodDisplay'
 const Home = () => {
   const[category,setCategory]=useState("All");
 
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
     <Header/>
     <ExploreMenu category={category} setCategory={setCategory}/>
+    <FoodDisplay category={category}/>
     </div>
   )
 }
