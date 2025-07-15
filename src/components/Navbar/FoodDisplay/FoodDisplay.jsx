e41711ef48e14aab085c1ad30978f0fc38941dd5
shivam847 <shivamsisodiya31072004@gmail.com>
2025-07-15 10:59:07 +0530

    footer addeed

--- src/components/Navbar/FoodDisplay/FoodDisplay.jsx
+++ src/components/Navbar/FoodDisplay/FoodDisplay.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../../context/StoreContext';
 import FoodItem from '../../FoodItem/FoodItem';
-const FoodDisplay = ({category}) => {
+const FoodDisplay = ({ category }) => {
 
     const { food_list } = useContext(StoreContext);
 
@@ -11,7 +11,9 @@ const FoodDisplay = ({category}) => {
             <h2>Top Dishes Near You</h2>
             <div className="food-display-list">
                 {food_list.map((item, index) => {
-                    return <FoodItem key={index} id={item.id} name={item.name} price={item.price} description={item.description} image={item.image} />
+                    if (category === "All" || category === item.category) {
+                        return <FoodItem key={index} id={item.id} name={item.name} price={item.price} description={item.description} image={item.image} />
+                    }
                 })}
             </div>
         </div>
--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,34 +1,36 @@
-import { createContext } from "react";
+import { createContext, useState,useEffect} from "react";
 
 export const StoreContext = createContext(null);
 import { food_list } from "../assets/assets";
 
 const StoreContextProvider = (props) => {
 
-    // const [cartItems, setCartItems] = useState({});
+    const [cartItems, setCartItems] = useState({});
 
-    // const addToCart = (itemId) => {
-    //     if (!cartItems[itemId]) {
-    //         setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
-    //     }
-    //     else {
-    //         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
-    //     }
-    // }
-    // const removeFromCart = (itemId) => {
-    //     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
-    // }
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
+        }
+        else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+        }
+    }
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
 
-    // useEffect(() => {
-    //     console.log(cartItems);
-    // }, [cartItems])
+
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems])
 
     const contextValue = {
-        food_list
-        // cartItems,
-        // setCartItems,
-        // addToCart,
-        // removeFromCart
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
     return <StoreContext.Provider value={contextValue}>
         {props.children}
